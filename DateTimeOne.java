350fe78aec020302d0795840462045fc217acd68
Jose Moreno <josemoreno@Joses-MacBook-Pro.local>
2019-10-22 13:02:42 -0500

    Added additional class and methods.

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,5 +1,46 @@
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
+   private int second;
    
+   public DateTimeOne (){
+	   second = 0;
+   }
+
+   @Override
+   int getValueOfSecond() {
+	   return second;
+   }
+
+   @Override
+   void dateTimeNow() {
+	   // TODO Auto-generated method stub
+	
+   }
+
+   @Override
+   void sleepForFiveSec() {
+	   // TODO Auto-generated method stub
+   }
+
+   @Override
+   void dateTimeOfOtherCity() {
+	   // TODO Auto-generated method stub
+   }
+
+   @Override
+   void dateTimeDifferentZone() {
+	   // TODO Auto-generated method stub
+   }
+
+   @Override
+   void timeZoneHashMap() {
+	   // TODO Auto-generated method stub
+   }
+   
+   
+   
+   
+   
+// end 
 }
---
+++ DateTimeTwo.java
@@ -0,0 +1,77 @@
+// Generated
+public class DateTimeTwo extends MesoDateTimeOneAbstract {
+	
+	private int second;
+	
+	// Constructor
+	public DateTimeTwo() {
+		second = 0;
+	}
+
+	
+	@Override
+	int getValueOfSecond() {
+		
+		return second;
+	}
+
+	@Override
+	void dateTimeNow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void sleepForFiveSec() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeOfOtherCity() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeDifferentZone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void timeZoneHashMap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void daysOfCurrentMonth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void daysOfAnyMonth(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void compareYear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void dateHashMap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void dateHashMapSorted() {
+		// TODO Auto-generated method stub
+		
+	}
+}
