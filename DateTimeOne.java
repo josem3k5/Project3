1cc4ece72bffbef109ebc92be0e58137538ae715
Jose Moreno <josemoreno@Joses-MacBook-Pro.local>
2019-10-25 12:38:43 -0500

    Finished and corrected DateTimeOne.java class

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -2,9 +2,14 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
@@ -18,6 +23,12 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 	private int second;
 	private long milli;
 	private LocalDateTime currTime = LocalDateTime.now();
+	LocalDateTime gmtTime;
+	LocalDateTime bstTime;
+	LocalDateTime cstTime;
+	String gmtTimeStr;
+	String bstTimeStr;
+	String cstTimeStr;
 	
 	// Constructor
 	public DateTimeOne () {
@@ -77,18 +88,18 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 		System.out.println("Time on Server: " + strServer);
 		
 		// For GMT time zone
-		LocalDateTime gmtTime = currTime.plusHours(5);
-		String gmtTimeStr = formatter.format(gmtTime);
+		gmtTime = currTime.plusHours(5);
+		gmtTimeStr = formatter.format(gmtTime);
 		System.out.println("GMT: " + gmtTimeStr);
 		
 		// For BST time zone
-		LocalDateTime bstTime = currTime.plusHours(6);
-		String bstTimeStr = formatter.format(bstTime);
+		bstTime = currTime.plusHours(6);
+		bstTimeStr = formatter.format(bstTime);
 		System.out.println("BST (90E): " + bstTimeStr);
 		
 		// For CST time zone
-		LocalDateTime cstTime = gmtTime.minusHours(5);
-		String cstTimeStr = formatter.format(cstTime);
+		cstTime = gmtTime.minusHours(5);
+		cstTimeStr = formatter.format(cstTime);
 		System.out.println("CST (90W): " + cstTimeStr);
 	}
 
@@ -99,23 +110,30 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
 		
 		// for GMT
-		LocalDateTime gmtTime = currTime.plusHours(5);
-		String gmtTimeStr = formatter.format(gmtTime);
+		gmtTime = currTime.plusHours(5);
+		gmtTimeStr = formatter.format(gmtTime);
 		System.out.println("GMT: " + gmtTimeStr);
 		
 		// for BST
-		LocalDateTime bstTime = currTime.plusHours(6);
-		String bstTimeStr = formatter.format(bstTime);
+		bstTime = currTime.plusHours(6);
+		bstTimeStr = formatter.format(bstTime);
 		System.out.println("BST: " + bstTimeStr); 
 		
 		// For CST time zone
-		LocalDateTime cstTime = gmtTime.minusHours(5);
-		String cstTimeStr = formatter.format(cstTime);
+		cstTime = gmtTime.minusHours(5);
+		cstTimeStr = formatter.format(cstTime);
 		System.out.println("CST: " + cstTimeStr);
+		
+		firstMap.put("GMT", gmtTimeStr);
+		firstMap.put("BST", bstTimeStr);
+		firstMap.put("CST", cstTimeStr);
 	}
 
 	@Override
 	void timeZoneHashMap() {
+		
+		// Previous DateTimeFormatter
+		DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
 		
 		String zst = "11/05/2018 10:59";
 		String ast = "10/01/2020 19:59";
@@ -123,12 +141,65 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 		firstMap.put("ZST", zst);
 		firstMap.put("AST", ast);
 		
+		// TreeMap to use for sorting date and time
+		TreeMap <String, String> sortedP1 = new TreeMap<> (firstMap);
+		
+		// Putting all data from PrintStyle1 into tree
+		sortedP1.putAll(firstMap);
+		
+		// For Print Style 1
 		System.out.println("Print Style 1:");
-		Iterator<String> itr = firstMap.keySet().iterator();
-		while (itr.hasNext()) {
-			System.out.println(itr.next());
+		for (String key : sortedP1.keySet()) {
+		    System.out.println(key + " " + firstMap.get(key));
 		}
 		
+		// For Print Style 3
+		secondMap.put(gmtTimeStr, null);
+		secondMap.put(bstTimeStr, null);
+		secondMap.put(cstTimeStr, null);
+		secondMap.put(zst, null);
+		secondMap.put(ast, null);
+		
+		TreeMap <String, String> sortedMap = new TreeMap<>(secondMap);
+		
+		// TreeMap to use for sorting date and time
+		Set <Entry <String, String>> sortedP2 =  sortedMap.entrySet();
+				
+		// Putting all data from PrintStyle1 into tree
+		sortedP1.putAll(secondMap);
+		
+		System.out.println("Print Style 3:");
+		for (Entry<String, String> key : sortedP2) {
+		    System.out.println(key.getKey());
+		}
+		
+		// For Print Style 5
+		System.out.println("Print Style 5: Final sorted Array:");
+		
+		// Array of size matching second hash map
+		LocalDateTime[] p5 = new LocalDateTime[secondMap.size()];
+		
+		// Converting String to LocalDateTime
+		LocalDateTime astTime = LocalDateTime.parse(ast, inputFormat);
+		LocalDateTime zstTime = LocalDateTime.parse(zst, inputFormat);
+		LocalDateTime gmtTime = LocalDateTime.parse(gmtTimeStr, inputFormat);
+		LocalDateTime bstTime = LocalDateTime.parse(bstTimeStr, inputFormat);
+		LocalDateTime cstTime = LocalDateTime.parse(cstTimeStr, inputFormat);
+		
+		// Assigning values to p5 array
+		p5[0] = astTime;
+		p5[1] = cstTime;
+		p5[2] = gmtTime;
+		p5[3] = bstTime;
+		p5[4] = zstTime;
+		
+		// Sorting array
+		Arrays.sort(p5, Collections.reverseOrder());
+		
+		// Loop through array to print contents
+		for(int i = 0; i < p5.length; i++) {
+			System.out.println(p5[i]);
+		}
 	}
 // end 
 }
